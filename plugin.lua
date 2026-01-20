7a66cb6344781e1e87b5df4d998fa0981fb2ba0d
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-01-20 14:53:32 -0600

    testing pharse
    Pharse is working, though needs improvements.
    Select notes and pharse(1-!); enable pharse(2-!); select a note and press 'Enter'. It will place bookmarks instead at the notes you pharsed rather than currently selected

--- plugin.lua
+++ plugin.lua
@@ -9,8 +9,11 @@ function draw()
 end
 --table setups
 object = {}--get start times for bookmarks
+pharseobject = {}
 words = {}--get words for bookmarks
 tablet = {}--table-t or tablet, name not yet related to use
+--varable setups
+local pharse_Switch = false
 -- UPPERCASING;FUNCTION_VARABLE, MiddleCasing;global_Varable, downcasing;local_varable/table
 -- Shortening;UPPERCASING
 function window_Shifter()
@@ -21,35 +24,74 @@ function window_Shifter()
  ---shhhh
 	drawing()
 	--give ID; get value
-	local askID = get("input", "")
 	---add visual buffer({x, y})
 	imgui.Dummy({100, 0})
 	--create button/s
 	button_Enter = imgui.Button("Enter", imgui_window_flags.Resize);
-	
-	---create clear button
-	if askID ~= "" then
-		imgui.SameLine(0, 0); 
+	imgui.SameLine(0, 0);
+	local button_Clear = false
+	if get("input", "") ~= "" then 
+		--create clear button
 		button_Clear = imgui.Button("Clear")
-		--askID placed after all other askID to have final say
-		if button_Clear then askID = "" end
 	end
+	imgui.SameLine(0, 0);
+	button_Pharse = imgui.Button("Pharse")
+	imgui.SameLine(0, 0);
+	if pharseobject[1] ~= nil then
+		imgui.Text("!")
+	else
+		imgui.TextDisabled("!")
+	end
+	imgui.SameLine(0, 0);
+	if pharse_Switch then
+		imgui.Text("!")
+	else
+		imgui.TextDisabled("!")
+	end
+
 	--create input auto resizeing:
 	liness = 1;
-	for _ in askID:gmatch("\n") do
+	for _ in get("input", ""):gmatch("\n") do
 		liness = liness + 1
 	end
 	---confused on how these mean, but do work quite well
 	maxLine = 0;
-	for liness in askID:gmatch("[^\n]*") do
+	for liness in get("input", ""):gmatch("[^\n]*") do
 		maxLine = math.max(maxLine, #liness)
 	end
 	size_X = math.max(180, maxLine * 8);
 	size_Y = math.max(20, liness * 18);
-	_, Asking = imgui.InputTextMultiline("", askID, 100000, {size_X, size_Y})
+	_, Asking = imgui.InputTextMultiline("", get("input", ""), 100000, {size_X, size_Y})
 	--:create input auto resizing
+	
+	if button_Pharse then
+		if #state.SelectedHitObjects ~= 0 then
+			Fed = 1 ; Fed_minor = 1 ; hitObject_minor = 1 ; pharseobject = {}
+			repeat
+			hitObject = state.SelectedHitObjects[Fed].StartTime
+			--if multiple hit objects are on the same time, skip all but one of them
+			if hitObject ~= hitObject_minor then
+				table.insert(pharseobject, hitObject)
+				Fed_minor = Fed_minor + 1
+			end
+	--2
+			hitObject_minor = state.SelectedHitObjects[Fed].StartTime
+	--3	
+			Fed = Fed + 1
+			until Fed == #state.SelectedHitObjects + 1
+			else
+			if #pharseobject ~= 0 then
+				pharse_Switch = tflipflop("Pharse Switch", false)
+			end
+		end
+	end
+
 	--set ID to input, give varable Asking as value
-	state.SetValue("input", Asking)
+	if button_Clear then state.SetValue("input", "") 
+	else state.SetValue("input", Asking) end
+	state.SetValue("Pharse Switch", pharse_Switch)
+	--askInput placed after all other askInput to have final say
+	
 	--:create input auto resizeing
 	--check if button is pressed
 	if button_Enter then
@@ -58,6 +100,7 @@ function window_Shifter()
 --		function pass
 		hallpass_section_0()---121
 	end
+	
 	--check inventory for hallpass #1
 	if Hallpass == 1 then
 --		function pass
@@ -68,6 +111,7 @@ function window_Shifter()
 --		function pass
 		hallpass_section_2()---162
 	end
+	
 --	function pass
 	resetbutton_section()---172
 	---end window script
@@ -76,6 +120,15 @@ end
 
 
 --functions:
+function tflipflop(ID, DEFAULT)
+	local current = state.GetValue(ID)
+	if current == nil then
+		current = DEFAULT or false
+	end
+	local next = not current
+	state.SetValue(ID, next)
+	return next
+end
 ---Get ID; Give value
 function get(IDENTIFIER, DEFAULTVALUE)
 	return state.GetValue(IDENTIFIER) or DEFAULTVALUE
@@ -128,29 +181,32 @@ function hallpass_section_0()
 		table.insert(words, #words+1, k)
 	end
 	Ready = "yes"
-	--num_hitobjects = the # of selected notes
-	num_Hitobjects = #state.SelectedHitObjects
 	--give hallpass #1 access
 	Hallpass = 1
 end
 function hallpass_section_1()
-	Fed = 1 ; Fed_minor = 1 ; phitObject = 1
+	--reset varables
+	Fed = 1 ; Fed_minor = 1 ; hitObject_minor = 1 ; table_num = 0
 	--1^insert to table, add bookmarks
 	--2^-set p-hitObject 1 note behind Fed
 	--2^cyle until varable = # selected notes
 	--1
-	repeat
-		hitObject = state.SelectedHitObjects[Fed].StartTime
-		--if multiple hit objects are on the same time, skip all but one of them
-		if hitObject ~= phitObject then
-			table.insert(object, utils.CreateBookmark(hitObject, words[Fed_minor]))
-			Fed_minor = Fed_minor + 1
-		end
+		repeat
+			if pharse_Switch then
+			pharsehitObject = pharseobject[Fed]
+			else pharsehitObject = state.SelectedHitObjects[Fed].StartTime end
+			hitObject = pharsehitObject
+			--if multiple hit objects are on the same time, skip all but one of them
+			if hitObject ~= hitObject_minor then
+				table.insert(object, utils.CreateBookmark(hitObject, words[Fed_minor]))
+				Fed_minor = Fed_minor + 1
+			end
 	--2
-		phitObject = state.SelectedHitObjects[Fed].StartTime
+			hitObject_minor = pharsehitObject
 	--3
-		Fed = Fed + 1
-	until Fed == #state.SelectedHitObjects + 1
+			Fed = Fed + 1
+			if pharse_Switch then table_num = #pharseobject else table_num = #pharseobject end
+		until Fed == table_num + 1
 	--if condisions are set and ready, contiue and create bookmark
 	if Ready == "yes" then
 		print("i!", Fed_minor-1 .. " bookmarks added")
